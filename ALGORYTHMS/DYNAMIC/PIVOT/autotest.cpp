3b5f1bd0a9eabda683a16fb6e2bca23e96039569
aashikhmin82 <aashikhmin82@gmail.com>
2015-09-01 00:37:27 +0300

    Refactoring

--- ALGORYTHMS/DYNAMIC/PIVOT/autotest.cpp
+++ ALGORYTHMS/DYNAMIC/PIVOT/autotest.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <cassert>
+#include <random>
+#include <ctime>
 
 #include "cut_method.h"
 
@@ -10,14 +12,45 @@ void check_result (size_t pivot_to_cut, vector<size_t> test_result, dynamic_db p
 {
     pivot_obj.cut(pivot_to_cut);
     vector<size_t> result = pivot_obj.print_result();
-    size_t indx = 0;
+    size_t index = 0;
     cout << "Check autotest result : " << endl;
     for (auto vec_el : result)
     {
-        cout << indx << " : " << vec_el << endl;
-        assert(vec_el == test_result[indx]);
-        indx++;
+        cout << index << " : " << vec_el << endl;
+        assert(vec_el == test_result[index]);
+        index++;
     }
+}
+
+void check_rand_result (size_t pivot_to_cut, dynamic_db pivot_obj)
+{
+    pivot_obj.cut(pivot_to_cut);
+    vector<size_t> result = pivot_obj.print_result();
+    size_t index = 0;
+    size_t pivot_res = 0;
+    cout << "Check Rand autotest result : " << endl;
+    for (auto vec_el : result)
+    {
+        cout << index << " : " << vec_el << endl;
+        pivot_res = pivot_res + vec_el;
+//        assert(vec_el == test_result[index]);
+        index++;
+    }
+
+    assert(pivot_to_cut == pivot_res);
+    cout << "Pivot Result : " << pivot_res << endl;
+}
+
+size_t nrand(size_t n)
+{
+    const size_t bucket_size = RAND_MAX / n;
+    size_t r = 0;
+
+    srand( (unsigned)time(NULL) );
+    do r = rand() / bucket_size;
+    while(r >= n);
+
+    return r;
 }
 
 int main()
@@ -46,6 +79,18 @@ int main()
     check_result(pivot_to_cut2, test_result2, pivot1);
     pivot1.erase_old_pivot();
 //    pivot1.cut(2);
+
+    prices = {0,1,3,5,8,9,10,17,17,20,24,30,26,27,28,29,30,31,32,33,34,35,36};
+//    size_t rand_size = nrand(22);
+    size_t rand_size = nrand(prices.size());
+    cout << "RAND SIZE : " << rand_size << endl;
+
+
+//Test 3: 
+    size_t pivot_to_cut3 = rand_size;
+//    vector<size_t> test_result2 = {2,2};
+    check_rand_result(pivot_to_cut3, pivot1);
+    pivot1.erase_old_pivot();
 
     return 0;
 }
--- ALGORYTHMS/DYNAMIC/PIVOT/cut_method.h
+++ ALGORYTHMS/DYNAMIC/PIVOT/cut_method.h
@@ -2,42 +2,50 @@
 #include <vector>
 #include <map>
 
+using price_t = size_t;
+using price_list_t = std::vector<price_t>;
+using pivot_t = size_t;
+using pivot_list_t = std::vector<pivot_t>;
+
 class dynamic_db
 {
     private:
-        std::vector <size_t> price;
-        std::vector <size_t> schem_plan;
-        std::vector <size_t> result_schem;
+//        std::vector <size_t> price;
+        price_list_t price;
+//        std::vector <size_t> scheme_plan;
+        pivot_list_t scheme_plan;
+//        std::vector <size_t> result_scheme;
+        pivot_list_t result_scheme;
         std::map<size_t, std::vector<size_t>> db;
         std::map<size_t, size_t> db_util;
 
     public:
         dynamic_db(const std::vector<size_t>& price_tab) : price(price_tab) {}
         
-        void print_price()
+        void print_price() const
         {
             size_t i=0;
-            for (auto price_val : price)
+            for (const auto price_val : price)
             {
                 std::cout << "[" << i << "] : " << price_val << std::endl;
                 ++i;
             }
         }
 
-        void print_scheme()
+        void print_scheme() const
         {
             std::cout << "Scheme : ";
-            for (auto part : schem_plan)
+            for (const auto& part : scheme_plan)
                 std::cout << part << " ";
 
             std::cout << "\n";
         }
 
-        std::vector<size_t> print_result()
+        std::vector<size_t> print_result() const
         {
             size_t result_sum = 0;
             std::cout << "Result Scheme : ";
-            for (auto part : result_schem)
+            for (const auto& part : result_scheme)
             {
                 result_sum += price[part];
                 std::cout << part << " ";
@@ -45,20 +53,26 @@ class dynamic_db
             std::cout << "\n";
 
             std::cout << "MAX Result : " << result_sum << std::endl;
-            return result_schem;
+            return result_scheme;
         }
 
         void erase_old_pivot()
         {
-            result_schem.erase(result_schem.begin(), result_schem.end());
+//            result_scheme.erase(result_schem.begin(), result_schem.end());
+            result_scheme.clear();
+            result_scheme.shrink_to_fit();
         }
 
         void debug_print_db()
         {
-            for (auto it = db.begin(); it != db.end(); ++it)
+//            for (auto it = db.begin(); it != db.end(); ++it)
+            for (const auto& db_el : db)
             {
-                std::cout << "[DEBUG] " << (*it).first << " : ";
-                for (auto el : db[(*it).first])
+//                std::cout << "[DEBUG] " << (*it).first << " : ";
+                std::cout << "[DEBUG] " << db_el.first << " : ";
+//                for (auto el : db[(*it).first])
+//                for (auto el : db[db_el->first])
+                for (const auto& el : db[db_el.first])
                 {
                     std::cout << el << " ";
                 }
@@ -72,18 +86,18 @@ class dynamic_db
 
             cut_long(length);
 
-            result_schem.push_back(schem_plan[0]);
-            if (schem_plan[1])
-                rest = schem_plan[1];
+            result_scheme.push_back(scheme_plan[0]);
+//            if (scheme_plan[1])
+            rest = scheme_plan[1];
 
             if (rest == 0)
             {
                 std::cout << "DB_UTIL : " << std::endl;
-                for (std::map<size_t, size_t>::const_iterator i = db_util.begin(), end = db_util.end(); i != end; ++i)
+//                for (std::map<size_t, size_t>::const_iterator i = db_util.begin(), end = db_util.end(); i != end; ++i)
+                for (const auto& i : db_util)
                 {
-                    std::cout << i->first << " : " << i->second << std::endl;
+                    std::cout << i.first << " : " << i.second << std::endl;
                 }
-                return 1;
             }
             else
             {
@@ -95,12 +109,21 @@ class dynamic_db
 
         size_t cut_long(size_t length, size_t nest=0)
         {
-            schem_plan.clear();
+            scheme_plan.clear();
             size_t cur_nest = nest;
             ++nest;
             size_t part1 = length, part2 = 0;
             size_t max_price = price[length];
             size_t price_debug = 0;
+
+            if (( db_util.find(length) != db_util.end() ) and (cur_nest != 0)) 
+            {
+
+                std::cout << "[DEBUG] A1!" << std::endl;
+                price_debug = db_util[length];
+                std::cout << "[DEBUG BUFFER] Max Price : " << price_debug << std::endl;
+                return price_debug;
+            }
 
             size_t tmp_price = 0;
             for (size_t first_cut = 1; first_cut < length; ++first_cut)
@@ -125,17 +148,16 @@ class dynamic_db
 
             if (cur_nest == 0)
             {
-                schem_plan.push_back(part1);
-                schem_plan.push_back(part2);
+                scheme_plan.push_back(part1);
+                scheme_plan.push_back(part2);
                 std::cout << "[DEBUG] L2 : " << length << " " << part1 << " " << part2 << std::endl;
                 db[length].push_back(part1);
                 db[length].push_back(part2);
             }
 
             if ( db_util.find(length) != db_util.end() )
-            {
                 price_debug = db_util[length];
-            }
+
             std::cout << "[DEBUG B] Price : " << max_price << "  Length : " << length << std::endl;
             std::cout << "[DEBUG B] Price_debug : " << price_debug << std::endl;
 
