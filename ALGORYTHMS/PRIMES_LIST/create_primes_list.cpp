aebf42409db5c44bac01b173aa06b961b6d2554b
Alexander Ashikhmin <aashikhmin82@gmail.com>
2016-08-30 00:38:24 +0300

    Creation of the result.md file was added. Time now is in mksec

--- ALGORYTHMS/PRIMES_LIST/create_primes_list.cpp
+++ ALGORYTHMS/PRIMES_LIST/create_primes_list.cpp
@@ -2,11 +2,12 @@
 #include <algorithm>
 #include <cassert>
 #include <chrono>
+#include <fstream>
 #include <iostream>
+#include <memory>
 #include <random>
 #include <set>
 #include <vector>
-#include <memory>
 
 #include "debug.h"
 #include "primes_v1.h"
@@ -96,37 +97,39 @@ void print_primes(const list_t& primes_list, const bool debug_flag, const string
 
 decltype(auto) check_func_duration(const number_t& max_num, const size_t func_amount, const bool debug_flag, debug_t& debug)
 {
+    typedef chrono::system_clock time;
+    typedef chrono::microseconds mks;
 
-    auto start_time = chrono::system_clock::now();
+    auto start_time = time::now();
 //    auto primes_list1 = create_primes_list1(max_num, debug);
     prime_numbers_v1 <number_t> primes1(max_num);
-    auto end_time = chrono::system_clock::now();
+    auto end_time = time::now();
 
     auto primes_list1 = primes1.get_primes_list();
     print_primes(primes_list1, debug_flag, "Function 1 (Check all numbers)");
 
-    using duration_t = decltype((end_time - start_time).count());
+    using duration_t = decltype(chrono::duration_cast<mks>(end_time - start_time).count());
     vector<duration_t> func_durations(func_amount);
-    func_durations[0] = (end_time - start_time).count();
+    func_durations[0] = chrono::duration_cast<mks>(end_time - start_time).count();
 
-    start_time = chrono::system_clock::now();
+    start_time = time::now();
 //    auto primes_list2 = create_primes_list2(max_num, debug);
     prime_numbers_v2 <number_t> primes2(max_num);
-    end_time = chrono::system_clock::now();
+    end_time = time::now();
 
     auto primes_list2 = primes2.get_primes_list();
     print_primes(primes_list2, debug_flag, "Function 2 (Using std::set)");
 
-    func_durations[1] = (end_time - start_time).count();
+    func_durations[1] = chrono::duration_cast<mks>(end_time - start_time).count();
 
-    start_time = chrono::system_clock::now();
+    start_time = time::now();
 //    auto primes_list3 = create_primes_list3(max_num, debug);
     prime_numbers_v3 <number_t> primes3(max_num);
-    end_time = chrono::system_clock::now();
+    end_time = time::now();
     auto primes_list3 = primes3.get_primes_list();
     print_primes(primes_list3, debug_flag, "Function 3 (std::vector<bool>)");
 
-    func_durations[2] = (end_time - start_time).count();
+    func_durations[2] = chrono::duration_cast<mks>(end_time - start_time).count();
 
     assert(primes_list1 == primes_list3);
     assert(primes_list2 == primes_list3);
@@ -159,23 +162,50 @@ int main(int argc, char** argv)
     //vector<number_t> max_num_list { 100, 500, 1000, 5000, 10000, 50000, 100000 };
     size_t func_amount { 3 };
 
+    string filename = "result.md";
+    ofstream result_file(filename, ios::out);
+
     cout << "Max Number\t" 
-        << "Function 1 (Check all numbers)\t"
-        << "Function 2 (Using std::set)\t"
-        << "Function 3 (std::vector<bool>)\t"
+        << "Function 1 - Check all numbers (mks)\t"
+        << "Function 2 - Using std::set (mks)\t"
+        << "Function 3 - std::vector<bool> (mks)\t"
         << "(Min Value)" << endl;
+
+    result_file << "Max Number | "
+        << "Function 1 - Check all numbers (mks) | "
+        << "Function 2 - Using std::set (mks) | "
+        << "Function 3 - std::vector<bool> (mks) | "
+        << "Fastest Function" << endl;
+
+    result_file << "-------------:|"
+        << "------------------------------------------:|"
+        << "------------------------------------------:|"
+        << "------------------------------------------:|"
+        << "-----------------------" << endl;
+
     for (const auto max_num : max_num_list)
     {
         auto func_durations = check_func_duration(max_num, func_amount, debug_flag, debug);
 
         auto minmax = minmax_element(func_durations.begin(), func_durations.end());
+        auto min_func_i = minmax.first - func_durations.begin();
+        vector <string> bold_font(3);
+        bold_font[min_func_i] = "**";
+
         cout << max_num << "\t"
             << func_durations[0] << "\t"
             << func_durations[1] << "\t"
             << func_durations[2] << "\t"
-            << "(Func " << (minmax.first - func_durations.begin() + 1) << ")" << endl;
+            << "(Func " << (min_func_i + 1) << ")" << endl;
+
+        result_file << max_num << " | ";
+        result_file << bold_font[0] << func_durations[0]<< bold_font[0] << " | ";
+        result_file << bold_font[1] << func_durations[1]<< bold_font[1] << " | ";
+        result_file << bold_font[2] << func_durations[2]<< bold_font[2] << " | ";
+        result_file << "Function " << min_func_i + 1 << endl;
     }
 
+    result_file.close();
 /*
     auto duration = func_durations[0];
     map<decltype(duration), string> function_time;
