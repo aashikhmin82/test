b24c17e5cbb96c9781037ce68d51b0c693165344
Alexander Ashikhmin <aashikhmin82@gmail.com>
2016-09-11 18:47:41 +0300

    Refactoring. Checks to prevent UB and unit tests were implemented

--- ALGORYTHMS/task2/randoms_list.h
+++ ALGORYTHMS/task2/randoms_list.h
@@ -1,33 +1,26 @@
 #pragma once
 
+#include <cassert>
 #include <iostream>
 #include <vector>
 #include <random>
 
 template <typename data_t>
-class randoms_list
+std::vector<data_t> randoms_list(const data_t& min, const data_t& max, const size_t size)
 {
-    private:
-    std::vector<data_t> list;
+    std::default_random_engine generator((std::random_device())());
+    std::uniform_int_distribution<data_t> random_size(min, max);
 
-    public:
-    randoms_list(const data_t& min, const data_t& max, const size_t size)
+//    std::cout << "[random_list] (" << min << "," << max << ") RANDOM LIST : ";
+
+    assert (size >= 1);
+    std::vector<data_t> list(size);
+    for (auto& list_element : list)
     {
-        std::default_random_engine generator((std::random_device())());
-        std::uniform_int_distribution<data_t> random_size(min, max);
-
-        std::cout << "[random_list] (" << min << "," << max << ") RANDOM LIST : ";
-        for (size_t i = 0; i < size; ++i)
-        {
-            auto rand_num = random_size(generator);
-            list.push_back(rand_num);
-            std::cout << rand_num << " ";
-        }
-        std::cout << std::endl;
+        list_element = random_size(generator);
+ //       std::cout << list_element << " ";
     }
+//    std::cout << std::endl;
 
-    std::vector<data_t> get()
-    {
-        return list;
-    }
-};
+    return list;
+}
--- ALGORYTHMS/task2/reverse_list.cpp
+++ ALGORYTHMS/task2/reverse_list.cpp
@@ -23,7 +23,7 @@ void create_list(first_object_t& first_element, const rnd_list_t& rnd_list)
 }
 
 template <typename first_object_t>
-void print_list(first_object_t& first_element)
+void print_list(const first_object_t& first_element)
 {
     cout << "[Print_list] Check List : " << flush;
     auto element = first_element;
@@ -40,32 +40,33 @@ void print_list(first_object_t& first_element)
 template <typename first_object_t>
 first_object_t reverse_v1(first_object_t& first_element)
 {
-    auto priv_element = first_element;
-    auto current = first_element->next();
-    auto next_element = first_element->next()->next();
+    if (!first_element)
+        return first_element;
 
-    priv_element->next() = nullptr;
+    auto prev_el = first_element;
+    auto current_el = first_element;
 
-    while (next_element->next())
+    while (current_el)
     {
-        current->next() = priv_element;
-        priv_element = current;
-        current = next_element;
-        next_element = current->next();
+        auto next_el = current_el->next();
+        current_el->next() = prev_el;
+        prev_el = current_el;
+        current_el = next_el;
     }
 
-    current->next() = priv_element;
-    next_element->next() = current;
+    first_element->next() = nullptr;
 
-    return next_element;
+    return prev_el;
 }
 
 template <typename first_object_t>
 first_object_t reverse_v2(first_object_t& first_element)
 {
+    if (!first_element or !first_element->next())
+        return first_element;
+
     auto current = first_element;
-    auto next = first_element->next();
-    auto new_first_element = reverse_two_nodes(current, next);
+    auto new_first_element = reverse_two_nodes(current, current->next());
 
     first_element->next() = nullptr;
 
@@ -83,27 +84,81 @@ object_t reverse_two_nodes(object_t& current_element, object_t& next_element)
     return new_first_element;
 }
 
+template <typename first_object_t>
+vector<number_t> dump_to_vector(const first_object_t& first_element)
+{
+    auto element = first_element;
+    vector<number_t> dump_list;
+    while (element)
+    {
+        dump_list.push_back(element->get_value());
+        element = element->next();
+    }
+
+    return dump_list;
+}
+
+template <typename obj_t>
+bool check_reversed(const obj_t& init_first, const obj_t& reversed_first)
+{
+    vector<number_t> init_list = dump_to_vector(init_first);
+    vector<number_t> reversed_init_list = dump_to_vector(reversed_first);
+
+    size_t i { init_list.size() };
+    for (const auto& init_el : init_list)
+    {
+        --i;
+        if (init_el != reversed_init_list[i])
+        {
+            throw logic_error("Elements must to be equal");
+//            return false;
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
     number_t min_value { 1 };
     number_t max_value { 500 };
-    size_t list_size { 15 };
+//    size_t list_size { 15 };
+//    vector<size_t> list_sizes { 1, 2, 4, 6 };
 
-    randoms_list<number_t> rnd_list(min_value, max_value, list_size);
-    auto numbers_list = rnd_list.get();
+    for (size_t list_size = 1; list_size < 100; list_size *= 2)
+    {
+        auto numbers_list = randoms_list(min_value, max_value, list_size);
 
-    auto first_element_v1(make_shared<node<number_t>>(numbers_list[0]));
-    auto first_element_v2(make_shared<node<number_t>>(numbers_list[0]));
+        auto first_element_v1(make_shared<node<number_t>>(numbers_list[0]));
+        create_list(first_element_v1, numbers_list);
+        auto new_first_v1 = reverse_v1(first_element_v1);
 
-    create_list(first_element_v1, numbers_list);
-    create_list(first_element_v2, numbers_list);
-    print_list(first_element_v1);
+        //CHECK reverse_v1 begin
+        auto first_el(make_shared<node<number_t>>(numbers_list[0]));
+        create_list(first_el, numbers_list);
+        cout << "Test reverse_v1 (size : " << list_size << ") : " <<
+            (check_reversed(first_el, new_first_v1) ? "Passed" : "Failed") << endl;
+        //end;
 
+        auto first_element_v2(make_shared<node<number_t>>(numbers_list[0]));
+        create_list(first_element_v2, numbers_list);
+        auto new_first_v2 = reverse_v2(first_element_v2);
+
+        //CHECK reverse_v2 begin
+        cout << "Test reverse_v2 (size : " << list_size << ") : " <<
+            (check_reversed(first_el, new_first_v2) ? "Passed" : "Failed") << endl;
+        //end;
+    }
+
+    //CHECK Nullptr begin
+    cout << "Func 1 -> Test nullptr : ";
+    shared_ptr<node<number_t>> first_element_v1 = nullptr;
     auto new_first_v1 = reverse_v1(first_element_v1);
-    cout << "Reverse V1 : ";
-    print_list(new_first_v1);
+    cout << ( !new_first_v1 ? "Passed" : "Failed" )<< endl;
 
+    cout << "Func 2 -> Test nullptr : ";
+    shared_ptr<node<number_t>> first_element_v2 = nullptr;
     auto new_first_v2 = reverse_v2(first_element_v2);
-    cout << "Reverse V2 : ";
-    print_list(new_first_v2);
+    cout << ( !new_first_v2 ? "Passed" : "Failed" ) << endl;
+    //End
 }
