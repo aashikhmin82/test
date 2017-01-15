8ea2a24e79b16c0bdfdd3e0ffdc14eacbb563273
Alexander Ashikhmin <aashikhmin82@gmail.com>
2017-01-16 00:06:57 +0300

    Refactoring after review

--- ALGORYTHMS/sort_multithreads/sort.cpp
+++ ALGORYTHMS/sort_multithreads/sort.cpp
@@ -5,17 +5,12 @@
 
 using namespace std;
 
-bool compare(const size_t& i, const size_t& j)
-{
-        return i < j;
-}
-
 int main() {
     vector<size_t> test_vec { 10,4,8,2,5,1,6,7,3,21,15,17,44,40 };
 
     cout << "Test Vector : " << endl;
-    for (const auto vec_node : test_vec) {
-        cout << vec_node << " ";
+    for (const auto vec_el : test_vec) {
+        cout << vec_el << " ";
     }
     cout << endl;
 
@@ -23,8 +18,8 @@ int main() {
     my_multithread_sort(test_vec, thread_amount);
 
     cout << "Sorted Vector : " << endl;
-    for (const auto vec_node : test_vec) {
-        cout << vec_node << " ";
+    for (const auto vec_el : test_vec) {
+        cout << vec_el << " ";
     }
     cout << endl;
 
